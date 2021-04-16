b43fb4ffa3b1d006b77fd823ff440b3e1b0a1e5e
adamzukerman <adamzukerman@gmail.com>
2021-04-15 22:09:43 -0400

    not relevant to project anymore

--- pyhop2/blocks_limited_stacks.py
+++ pyhop2/blocks_limited_stacks.py
@@ -1,9 +1,14 @@
+"""
+This file is not really relevant to the project anymore 
+since it doesn't use the procedually generated methods.
+"""
+
 import pyhop2
 
 pyhop2.Domain('blocks_limited_stacks')
 import blocks_limited_stacks_actions as act
 import blocks_limited_stacks_methods
-import blocks_limited_stacks_goal_methods
+#import blocks_limited_stacks_goal_methods
 import blocks_generator as gen
 import blocks_limited_stacks_top_level_methods
 
@@ -23,8 +28,8 @@ def simple_state():
     'e': 'c', 'f': 'e', 'g': 'f', 'h': 'table'}
     s0.clear = {'a': False, 'b': True, 'c': False, 'd': True, 'e': False,
     'f': False, 'g': True, 'h': True}
-    s0.stacks = [['a','c','e','f','g'], ['b'], ['d'], ['h'], [], []]
-    s0.max_stacks = 6
+    s0.stacks = [['a','c','e','f','g'], ['b'], ['d'], ['h']]
+    s0.max_stacks = 4
     return s0
 
 def small_state():
@@ -44,7 +49,7 @@ def small_goal():
 
 
 """state/goal generation parameters"""
-num_blocks = 5
+num_blocks = 7
 stack_prob = .5
 max_stacks = 6
 
@@ -75,25 +80,22 @@ blocks from the initial state have a declared position will work.
 You can either pass a [multigoal] as the todo
 or you can pass [('solve_goal', s0, goal_state)] as the todo
 """
-print(pyhop2.find_plan(s_rand, [g]))
 print(pyhop2.find_plan(s_rand, [('solve_goal', g_rand)]))
-print(gen.gen_list_representation(s_rand, max_stacks))
-print(gen.gen_list_representation(g, max_stacks))
+# print(gen.gen_list_representation(s_rand, max_stacks))
+# print(gen.gen_list_representation(g, max_stacks))
 
 
 """Some simple tests I used for debugging"""
-# s0 = simple_state()
-# print(act.get_block_above(s_small, '1'))
-# print(pyhop2.find_plan(s0, [('make_clear_distribute', 'a', 'd')]))
-# print(pyhop2.find_plan(s0, [('make_clear_dump_to_smallest', 'a', 'd')]))
-# print(pyhop2.find_plan(s0, [('make_clear_split_to_nbr', 'a', 'c')]))
-# print(pyhop2.find_plan(s0, [('make_clear_split_to_smallest', 'a', 'b')]))
-# print(pyhop2.find_plan(s0, [('make_clear_split_to_nbr', 'a', 'd')]))
-# print(pyhop2.find_plan(s0, [('pos', 'a', 'd')]))
-# print(pyhop2.find_plan(s0, [('pos', 'a', 'c')]))
-# print(pyhop2.find_plan(s_small, [('pos', '0', 'table'), ('pos', '1', '0')]))
-# print(pyhop2.find_plan(s_small, [('pos', '0', 'table')]))
-# plan =pyhop2.find_plan(s_small, [('pos', '0', 'table')])
-# state = _apply_plan(s_small, plan)
-# print(state.stacks)
-# print(pyhop2.find_plan(state, [('pos', '2', '0')]))
+s0 = simple_state()
+
+s0 = pyhop2.State()
+s0.pos = {'a': 'table', 'b': 'table', 'c': 'a', 'd': 'table', 'e': 'table'}
+s0.clear = {'a': False, 'b': True, 'c': True, 'd': True, 'e': True}
+s0.stacks = [['a','c'], ['b'], ['d'], ['e']]
+s0.max_stacks = 4
+
+g_dummy = s0.copy()
+g_dummy.clear = {}
+g_dummy.pos['c'] = 'table'
+g_dummy.pos['b'] = 'a'
+print(pyhop2.find_plan(s0, [('solve_goal', g_dummy)]))
